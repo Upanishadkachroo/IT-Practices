e3cd8c3e55f34bf0569a5ad99bccdcea7b9ea785
Upanishadkachroo <upanishadkachroo@gmail.com>
2025-08-01 00:28:30 +0530

    gcd

---
+++ CNS/gcd.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int GCD(int a, int b){
+    if(a==0){
+        return b;
+    }
+    return GCD(b%a, a);
+}
+
+int main(){
+    int a, b;
+    cin>>a>>b;
+
+    int g=GCD(a, b);
+    cout<<g<<endl;
+}
